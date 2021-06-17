df129cae7ae7234d28438d1fe03ef73cab20c7c5
Sofya Tuymedova <stuymedova@gmail.com>
2021-06-18 01:05:06 +0300

    refactor: add storage folder

--- public/index.php
+++ public/index.php
@@ -1,13 +1,18 @@
 <?php
 
-require '../kirby/bootstrap.php';
+include __DIR__ . '/../vendor/autoload.php';
 
 $kirby = new Kirby([
   'roots' => [
-    'index'   => __DIR__,
-    'content' => __DIR__ . '/../content',
-    'site'    => __DIR__ . '/../site',
-  ],
+    'index'    => __DIR__,
+    'base'     => $base    = dirname(__DIR__),
+    'content'  => $base . '/content',
+    'site'     => $base . '/site',
+    'storage'  => $storage = $base . '/storage',
+    'accounts' => $storage . '/accounts',
+    'cache'    => $storage . '/cache',
+    'sessions' => $storage . '/sessions',
+  ]
 ]);
 
 echo $kirby->render();

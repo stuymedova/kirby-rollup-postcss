de74813301b014c08b78306789cdb9329dfe0c36
Sofya Tuymedova <stuymedova@gmail.com>
2021-06-17 09:35:50 +0300

    feat: add robots.txt and sitemap.xml

--- site/config/config.php
+++ site/config/config.php
@@ -2,4 +2,13 @@
 
 return [
   'smartypants' => true,
+  'bnomei.robots-txt.sitemap' => '/sitemap.xml',
+  'bnomei.robots-txt.groups' => [
+    '*' => [
+      'disallow' => [
+        '/panel/',
+      ],
+    ],
+  ],
+  'omz13.xmlsitemap.disableImages' => true,
 ];
